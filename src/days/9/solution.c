4e300da10795ed23a3cf0c9daac0d6e1f8877107
ghlmtz <devilishgiant@gmail.com>
2024-12-09 01:34:49 -0500

    Day 9 part 2 done but also not happy

--- src/days/9/solution.c
+++ src/days/9/solution.c
@@ -1,7 +1,96 @@
 #include "advent.h"
 
+typedef struct mem_block {
+    int idx;
+    int len;
+    struct mem_block *prev;
+    struct mem_block *next;
+} MemoryBlock;
+
 static long part1 = 0;
 static long part2 = 0;
+
+static void parse_line2(char *line)
+{
+    MemoryBlock *head = NULL;
+    MemoryBlock *tail = NULL;
+    int idx = 0;
+    int line_idx = 0;
+    int count = 0;
+    while (line[line_idx] != '\0') 
+    {
+        MemoryBlock *blk = malloc(sizeof(MemoryBlock));
+        blk->idx = count % 2 == 0 ? idx : -1;
+        blk->len = line[line_idx] - '0';
+        blk->next = NULL;
+        if (head == NULL) {
+            head = blk;
+            blk->prev = NULL;
+            tail = blk;
+        }
+        else {
+            tail->next = blk;
+            blk->prev = tail;
+            tail = blk;
+        }
+        if (count % 2  == 0)
+            idx++;
+        line_idx++;
+        count++;
+    }
+
+    MemoryBlock *end = tail;
+    MemoryBlock *start = head;
+    int dec_id = end->idx;
+    while (end != NULL && dec_id > 0)
+    {
+        MemoryBlock *prev = NULL;
+        if (end->idx != dec_id)
+        {
+            end = end->prev;
+            continue;
+        }
+        while (head->idx > -1 || head->len < end->len)
+        {
+            prev = head;
+            head = head->next;
+            if (head == NULL || head == end) 
+            {
+                goto nogood;
+            }
+        }
+        if (head->len == end->len)
+        {
+            head->idx = end->idx;
+            end->idx = -1;
+        }
+        else
+        {
+            MemoryBlock *new_blk = malloc(sizeof(MemoryBlock));
+            new_blk->idx = end->idx;
+            new_blk->len = end->len;
+            if (prev != NULL)
+                prev->next = new_blk;
+            new_blk->prev = prev;
+            new_blk->next = head;
+            head->len = head->len - end->len;
+            end->idx = -1;
+        }
+    nogood:
+        dec_id--;
+        end = end->prev;
+        head = start;
+    }
+    count = 0;
+    while (start != NULL)
+    {
+        for (int i = 0; i < start->len; i++, count++) {
+            if (start->idx != -1)
+                part2 += count * start->idx;
+        }
+        start = start->next;
+    }
+}
 
 static void parse_line(char *line)
 {
@@ -54,13 +143,15 @@ static void parse_line(char *line)
 
     free(enormous);
     free(backwards);
+
+    parse_line2(line);
 }
 
 int day9()
 {
     READ_INPUT("input");
     for_each_line(parse_line);
-    printf("\n%ld\n", part1);
+    printf("%ld\n", part1);
     printf("%ld\n", part2);
     return 0;
 }
