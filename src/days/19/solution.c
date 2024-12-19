811d57a7d11acbe0d0e25dca7627b65f9118f4ab
ghlmtz <devilishgiant@gmail.com>
2024-12-19 00:56:44 -0500

    Day 19 done

--- src/days/19/solution.c
+++ src/days/19/solution.c
@@ -1,23 +1,55 @@
 #include "advent.h"
 #include "dyn_arr.h"
+#include "hashmap.h"
 
-DynArr *towels;
-static int part1 = 0;
+char *towels[500];
+int n_towels = 0;
+static long part1 = 0;
+static long part2 = 0;
 
-static int try_towels(char *design)
+struct subdesign
 {
-    if (*design == '\0')
-        return 1;
-    for (size_t i = 0; i < towels->length; i++)
-    {
-        char *towel = dyn_arr_get(towels, i);
-        if(strncmp(towel, design, strlen(towel)) == 0)
-            if(try_towels(design + strlen(towel)))
-                return 1;
-    }
-    return 0;
+    char *design;
+    long arrange;
+};
+HashMap *designs;
+
+static int _strcmp(void *p, void *q)
+{ 
+    return strcmp(*(char **)p, *(char **)q) == 0; 
 }
 
+unsigned subdesign_hash(void *p)
+{
+    struct subdesign *z = (struct subdesign *)p;
+    return (HASH_SIZE + (z->design[0] * 57 + z->design[1] * 31 + z->design[2] * 17) % HASH_SIZE) % HASH_SIZE;
+}
+
+static long try_towels(char *design)
+{
+    long ret = 0;
+    struct subdesign test = {.design = design};
+    struct subdesign *subdesign = hash_exists(designs, &test);
+    if (subdesign != NULL)
+        return subdesign->arrange;
+    if (*design == '\0')
+        return 1;
+    for (int i = 0; i < n_towels; i++)
+    {
+        char *towel = towels[i];
+        if (towel[0] < design[0])
+            continue;
+        if (towel[0] > design[0])
+            break;
+        if(strncmp(towel, design, strlen(towel)) == 0)
+            ret += try_towels(design + strlen(towel));
+    }
+    subdesign = malloc(sizeof(struct subdesign));
+    subdesign->design = design;
+    subdesign->arrange = ret;
+    hash_add(designs, subdesign);
+    return ret;
+}
 
 static void parse_line(char *line)
 {
@@ -27,23 +59,27 @@ static void parse_line(char *line)
         char *add_me = strtok(line, ", ");
         while(add_me != NULL)
         {
-            dyn_arr_add(towels, add_me);
+            towels[n_towels++] = add_me;
             add_me = strtok(NULL, ", ");
         }
+        sort_strings(towels, n_towels);
     }
     if (line_num > 1)
     {
-        part1 += try_towels(line);
+        long result = try_towels(line);
+        part2 += result;
+        if (result > 0) part1++;
     }
     line_num++;
 }
 
 int day19()
 {
-    towels = dyn_arr_alloc(sizeof(char *));
+    designs = hash_init(subdesign_hash, _strcmp, free);
     READ_INPUT("input");
     for_each_line(parse_line);
-    printf("%d\n", part1);
-    dyn_arr_free(towels);
+    printf("%ld\n", part1);
+    printf("%ld\n", part2);
+    hash_free(designs);
     return 0;
 }
