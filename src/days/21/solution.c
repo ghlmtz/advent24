bfa366652c645dcd23268b04da4b5a2c51b286ef
ghlmtz <devilishgiant@gmail.com>
2024-12-21 18:22:26 -0500

    Day 21.2 done

--- src/days/21/solution.c
+++ src/days/21/solution.c
@@ -1,68 +1,88 @@
 #include "advent.h"
 #include "day21.h"
 
-#define STRING_SIZE 25600
-
-static size_t d_recurse(const char *line, int idx, char *s, size_t s_idx, int depth)
+static void n_recurse(const char *line, int idx, char *s, int s_idx)
 {
     if (*line == '\0')
     {
         s[s_idx] = '\0';
-        if (depth == 1)
-        {
-            return strlen(s);
-        }
-        else
-        {
-            char d[STRING_SIZE] = {0};
-            return d_recurse(s, 4, d, 0, depth + 1);
-        }
+        return;
     }
-    int new_idx;
-    switch(*line)
-    {
-        case '^':
-            new_idx = 0;
-            break;
-        case 'v':
-            new_idx = 1;
-            break;
-        case '<':
-            new_idx = 2;
-            break;
-        case '>':
-            new_idx = 3;
-            break;
-        default:
-            new_idx = 4;
-            break;
-    }
-    
-    char *move = d_keypad[idx][new_idx][0];
-    strcpy(s + s_idx, move);
-    return d_recurse(line + 1, new_idx, s, s_idx + strlen(move), depth);
-}
-
-static size_t n_recurse(const char *line, int idx, char *s, int s_idx)
-{
-    if (*line == '\0')
-    {
-        s[s_idx] = '\0';
-        char d[STRING_SIZE] = {0};
-        return d_recurse(s, 4, d, 0, 0);
-    }
-    int new_idx = *line == 'A' ? 10 : *line - '0';
-
+    int new_idx = *line - '0';
+    if (*line == 'A')
+        new_idx = 10;
     char *move = n_keypad[idx][new_idx][0];
     strcpy(s + s_idx, move);
-    return n_recurse(line + 1, new_idx, s, s_idx + strlen(move));
+    n_recurse(line + 1, new_idx, s, s_idx + strlen(move));
 }
 
-int part1 = 0;
+static int get_new_idx(char ch)
+{
+    switch(ch) {
+        case '^':
+            return 0;
+        case 'v':
+            return 1;
+        case '<':
+            return 2;
+        case '>':
+            return 3;
+        default:
+            return 4;
+    }
+}
+
+long part2 = 0;
+long do_moves(const long *old_counts_ptr, char *line, int depth)
+{
+    long sum = 0;
+    long *old_counts = calloc(25, sizeof(long));
+    memcpy(old_counts, old_counts_ptr, sizeof(long)*25);
+    while(depth > 0)
+    {
+        long *new_counts = calloc(25, sizeof(long));
+        for (int idx = 0; idx < 5; idx++)
+        {
+            for (int new_idx = 0; new_idx < 5; new_idx++)
+            {
+                char *move = d_keypad[idx][new_idx][0];
+                int move_idx = 4;
+                while (*move != '\0')
+                {
+                    int move_new_idx = get_new_idx(*move);
+                    new_counts[5 * move_idx + move_new_idx] += old_counts[5 * idx + new_idx];
+                    move_idx = move_new_idx;
+                    move++;
+                }
+            }
+        }
+        free(old_counts);
+        old_counts = new_counts;
+        depth--;
+    }
+    for(int i = 0; i < 25; i++)
+        sum += old_counts[i] * atoi(line);
+    free(old_counts);
+    return sum;
+}
+
+long part1 = 0;
 static void parse_line(char *line)
 {
-    char s[STRING_SIZE] = {0};
-    part1 += n_recurse(line, 10, s, 0) * atoi(line);
+    char s[256] = {0};
+    long counts[25] = {0};
+    n_recurse(line, 10, s, 0);
+    int idx = 4;
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        int new_idx = get_new_idx(s[i]);
+        counts[5*idx + new_idx]++;
+        idx = new_idx;
+        i++;
+    }
+    part1 += do_moves(counts, line, 2);
+    part2 += do_moves(counts, line, 25);
 }
 
 int day21()
@@ -70,7 +90,8 @@ int day21()
     READ_INPUT("input");
     for_each_line(parse_line);
 
-    printf("%d\n", part1);
+    printf("%ld\n", part1);
+    printf("%ld\n", part2);
 
     return 0;
 }
