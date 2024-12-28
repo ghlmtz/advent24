7a5136d49a4217bd36f9b8b77bfae428de63b834
ghlmtz <devilishgiant@gmail.com>
2024-12-28 10:11:14 -0500

    Day 23.2

--- src/days/23/solution.c
+++ src/days/23/solution.c
@@ -20,6 +20,62 @@ static void parse_line(char *line)
         link[node2][node1] = 1;
 }
 
+int found_long = 0;
+
+void bron_kerbosch(int *r, int *p, int *x, int n_r)
+{
+    int empty = 1;
+    for (int i = 0; i < ALPHA; i++)
+    {
+        if (p[i] != 0 || x[i] != 0) {
+            empty = 0;
+            break;
+        }
+    }
+    if (empty) {
+        if (n_r > 12)
+        {
+            char print_me[100] = {0};
+            int ch = 0;
+            for (int i = 0; i < ALPHA; i++)
+            {
+                if (r[i]) {
+                    sprintf(print_me + (ch++) * 3, "%c%c,", i / 26 + 'a', i % 26 + 'a');
+                }
+            }
+            print_me[ch * 3 - 1] = '\0';
+            printf("%s\n", print_me);
+            found_long = 1;
+        }
+        return;
+    }
+    for (int i = 0; i < ALPHA; i++)
+    {
+        if (!p[i])
+            continue;
+        r[i] = 1;
+        int *new_p = calloc(ALPHA, sizeof(int));
+        int *new_x = calloc(ALPHA, sizeof(int));
+        for (int n = 0; n < ALPHA; n++)
+        {
+            if (adj[i][n]) {
+                if (p[n])
+                    new_p[n] = 1;
+                if (x[n])
+                    new_x[n] = 1;
+            } 
+        }
+        bron_kerbosch(r, new_p, new_x, n_r + 1);
+        free(new_p);
+        free(new_x);
+        r[i] = 0;
+        p[i] = 0;
+        x[i] = 1;
+        if (found_long)
+            return;
+    }
+}
+
 int day23()
 {
     READ_INPUT("input");
@@ -34,6 +90,9 @@ int day23()
         {
             if (!present[y])
                 continue;
+            int check1 = link[y][t_node] | link[t_node][y];
+            if (!check1)
+                continue;
             for (int z = y + 1; z < ALPHA; z++)
             {
                 if (!present[z])
@@ -42,14 +101,18 @@ int day23()
                     continue;
                 if (z / 26 == 19 && z < t_node)
                     continue;                    
-                int check1 = link[y][t_node] | link[t_node][y];
                 int check2 = link[z][t_node] | link[t_node][z];
                 int check3 = link[y][z];
-                if (check1 && check2 && check3)
+                if (check2 && check3)
                     count++;
             }
         }
     }
     printf("%d\n", count);
+    int *r = calloc(ALPHA, sizeof(int));
+    int *x = calloc(ALPHA, sizeof(int));
+    bron_kerbosch(r, present, x, 0);
+    free(r);
+    free(x);
     return 0;
 }
